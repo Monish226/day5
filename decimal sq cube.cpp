4c2a3589bed278be61e182167388cf8e692cee18
Monish <112949411+Monish226@users.noreply.github.com>
2022-09-16 11:23:26 +0530

    Add files via upload

---
+++ decimal sq cube.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ float num;
+ int square,cube;
+ cout<<("Given Number : ");
+ cin>>num;
+ cout<<("Square Number : %0.2f\n",num*num)<<endl;
+ cout<<("cube Number : %0.3f\n",num*num*num)<<endl;
+}
